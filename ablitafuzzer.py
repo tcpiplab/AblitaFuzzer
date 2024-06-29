f3136cb834607576ecb3b1d168b8ad9e633af4b3
tcpiplab <lshep.usc@gmail.com>
2024-06-29 12:47:37 -0700

    move test call functions to test file and dir

--- ablitafuzzer.py
+++ ablitafuzzer.py
@@ -1,59 +1,6 @@
 import argparse
-from openai import OpenAI
-import requests
-import json
 
-def test_call_abliterated_model():
-
-    # Point to the local server
-    client = OpenAI(base_url="http://localhost:8181/v1", api_key="lm-studio")
-
-    completion = client.chat.completions.create(
-        # model="failspy/Meta-Llama-3-8B-Instruct-abliterated-v3-GGUF",
-        model="failspy/Phi-3-medium-4k-instruct-abliterated-v3",
-        messages=[
-            {"role": "system",
-             "content": "Below is an instruction that describes a task. Write a response that appropriately completes the request."},
-            {"role": "user", "content": "Introduce yourself."}
-        ],
-        temperature=0.7,
-    )
-
-    print(completion.choices[0].message.content)
-
-
-def test_call_target_model():
-
-    # Define the URL for the API
-    url = "http://localhost:11434/api/chat"
-
-    # Define the payload
-    payload = {
-        "model": "mistral",
-        "messages": [
-            {
-                "role": "user",
-                "content": "why is the sky blue?"
-            }
-        ],
-        "stream": False
-    }
-
-    # Set the headers
-    headers = {
-        "Content-Type": "application/json"
-    }
-
-    # Send the POST request
-    response = requests.post(url, headers=headers, data=json.dumps(payload))
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Print the response from the server
-        print("Response:", response.json())
-    else:
-        # Print the error
-        print(f"Error: {response.status_code} - {response.text}")
+from tests.test_calling_apis import test_call_abliterated_model, test_call_target_model
 
 
 def main():
